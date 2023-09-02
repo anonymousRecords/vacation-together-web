19b7faec1efaf378c53194c5e78e3bfef1ba329e
Chohyejin53 <hyejin2338@gmail.com>
2023-09-02 19:41:56 +0900

    아이콘 수정 및 input radio 수정

--- src/MakePage/components/Radio.tsx
+++ src/MakePage/components/Radio.tsx
@@ -22,8 +22,10 @@ export default function Radio({ groupName } :RadioProps) {
 const ToggleBox = styled.div`
   width: 328px;
   height: 45px;
+  margin-bottom: 10px;
   display: flex;
-  gap: 10px;
+  gap: 15px;
+
 `;
 
 const Toggle = styled.div`
@@ -31,11 +33,34 @@ const Toggle = styled.div`
   font-family: Noto Sans KR;
   font-size: 20px;
   font-style: normal;
-  font-weight: 700;
+  font-weight: 400;
   line-height: normal;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   gap: 5px;
+
+
+  /* 체크된 상태일 때 라디오 버튼 색상 변경 */
+  input[type="radio"]:checked + label::before {
+    background-color: #33FF00;
+  }
+
+  input[type="radio"] {
+    width: 20px; 
+    height: 20px; 
+    appearance: none; 
+    border: 2px solid #000; 
+    border-radius: 50%; 
+    outline: none; 
+  }
+
+  /* 체크된 상태 */
+  input[type="radio"]:checked {
+    background-color: #33FF00; 
+    border-color: #000;
+  }
+};
+
 `;
