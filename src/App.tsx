3d0664632c515fced6f81a118064db538897f262
anonymousrecords <a.Records2022@gmail.com>
2023-09-02 18:01:19 +0900

    feat : MakePage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StartPage from "./StartPage/StartPage";
-import SchedulePage from "./SchedulePage/SchedulePage";
+// import SchedulePage from "./SchedulePage/SchedulePage";
 import CreatePage from "./CreatePage/CreatePage";
 import EnterPage from "./EnterPage/EnterPage";
+import MakePage from "./MakePage/MakePage";
 
 const App: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<StartPage/>}/>
-            <Route path="/schedule" element={<SchedulePage/>}/>
-          <Route path="/create" element={<CreatePage/>}/>
-          <Route path="/enter" element={<EnterPage/>}/>
+          <Route path="/" element={<StartPage />} />
+          {/* <Route path="/schedule" element={<SchedulePage />} /> */}
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/enter" element={<EnterPage />} />
+          <Route path="/make" element={<MakePage/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/MakePage/MakePage.tsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import ModalOverlay from "./components/ModalOverlay";
+import { useState } from "react";
+import StartModal from "./components/StartModal";
+import Header from "../common/components/Header";
+import { useNavigate } from "react-router-dom";
+import Question from "./components/Question";
+
+const MakePage = () => {
+  // Start 모달창
+  const [showStartModal, setShowStartModal] = useState(true);
+
+  // const showStartModalHandler = () => {
+  //   setShowStartModal(true);
+  // };
+
+  const hideStartModalHandler = () => {
+    setShowStartModal(false);
+  };
+
+  // useNavigate
+  const navigate = useNavigate();
+
+  return (
+    <StyledMakePage>
+      <Header
+        headertitle="계약서 작성하기"
+        onBack={() => {
+          navigate("/enter");
+        }}
+      />
+      <Question />
+
+      {/* Start 모달창 */}
+      <ModalOverlay
+        blur
+        onHideModal={hideStartModalHandler}
+        show={showStartModal}
+      >
+        <StartModal onHideModal={hideStartModalHandler} />
+      </ModalOverlay>
+      <CompleteButton
+        onClick={() => {
+          navigate("/complete");
+        }}
+      >
+        작성 완료하기
+      </CompleteButton>
+    </StyledMakePage>
+  );
+};
+
+const StyledMakePage = styled.div`
+  min-width: 360px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CompleteButton = styled.button`
+  width: 313px;
+  height: 54px;
+  border-radius: 8px;
+  background: #000;
+  margin-top: 30px;
+
+  color: #fff;
+  text-align: center;
+  font-family: Noto Sans KR;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export default MakePage;
