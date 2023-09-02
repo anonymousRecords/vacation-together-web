40e480c4ab001fd836d8ecdc69fd142e4384ab43
anonymousrecords <a.Records2022@gmail.com>
2023-09-02 18:00:24 +0900

    feat : Header 구현

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,13 +1,37 @@
+import styled from "styled-components";
+
 type Props = {
-    onBack: () => void;
-}
-const Header = ({ onBack }: Props) => {
-    return <Container>
-        하이
+  onBack: () => void;
+  headertitle: string;
+};
+const Header = ({ onBack, headertitle }: Props) => {
+  return (
+    <Container>
+      <button onClick={onBack}>
+        <img src="/assets/icon/back-space.svg" />
+      </button>
+      {headertitle}
     </Container>
-}
+  );
+};
 export default Header;
 
 const Container = styled.div`
-padding: 1.2rem 1.6rem;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  padding: 16px 113px 16px 12px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #f0f0f0;
+
+  color: #000;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 `;
